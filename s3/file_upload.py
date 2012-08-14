6e92a26888187c69c957d0778759688b4eebe974
David Higgins <sligodave@gmail.com>
2012-08-14 16:15:29 -0600

    Updated to allow for import or commandline usage.

--- s3/file_upload.py
+++ s3/file_upload.py
@@ -1,19 +1,22 @@
-from sys import argv
-
 import boto
 from boto.s3.key import Key
 
-ACCESS_KEY = argv[1]
-SECRET_ACCESS_KEY = argv[2]
-BUCKET_NAME = argv[3]
-SOURCE_PATH = argv[4]
-DESTINATION_PATH = argv[5]
 
-conn = boto.connect_s3(ACCESS_KEY,
-                       SECRET_ACCESS_KEY)
-bucket = conn.get_bucket(BUCKET_NAME)
+def upload_file(ACCESS_KEY, SECRET_ACCESS_KEY, BUCKET_NAME, SOURCE, DESTINATION):
+    conn = boto.connect_s3(ACCESS_KEY, SECRET_ACCESS_KEY)
+    bucket = conn.get_bucket(BUCKET_NAME)
 
-print 'Uploading "%s" to "%s" in "%s"' % (SOURCE_PATH, DESTINATION_PATH, BUCKET_NAME)
-k = Key(bucket, DESTINATION_PATH)
-k.set_contents_from_filename(SOURCE_PATH, policy='public-read')
-print 'Uploaded'
+    k = Key(bucket, DESTINATION)
+    print 'Uploading "%s" to "%s" in "%s"' % (SOURCE, DESTINATION, BUCKET_NAME)
+    k.set_contents_from_filename(SOURCE, policy='public-read')
+    print 'Uploaded'
+
+
+if __name__ == "__main__":
+    from sys import argv
+    ACCESS_KEY = argv[1]
+    SECRET_ACCESS_KEY = argv[2]
+    BUCKET_NAME = argv[3]
+    SOURCE = argv[4]
+    DESTINATION = argv[5]
+    upload_file(ACCESS_KEY, SECRET_ACCESS_KEY, BUCKET_NAME, SOURCE, DESTINATION)
