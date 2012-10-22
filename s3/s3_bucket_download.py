1ff02f52ea0c5928f29c36830ab4acc039be5586
David Higgins <sligodave@gmail.com>
2012-10-22 12:01:59 -0600

    Fixed the command line arguments setting. Also the conversion of the dst path to an abs path.

--- s3/s3_bucket_download.py
+++ s3/s3_bucket_download.py
@@ -14,11 +14,10 @@ def download_to_path(dst_path,
     # Get the bucket to copy from
     conn = boto.connect_s3(access_key, secret_access_key)
     bucket = conn.get_bucket(bucket_name)
-
+    dst_path = abspath(dst_path)
     keylist = bucket.list(prefix=src_path)
     for key in keylist:
         dst_file_path = join(dst_path, key.name)
-        dst_file_path = abspath(dst_file_path)
         dst_directory_path = dirname(dst_file_path)
         if exists(dst_file_path):
             if not overwrite:
@@ -41,10 +40,10 @@ if __name__ == "__main__":
     # AWS Bucket Name
     bucket_name = argv[4]
     # AWS Source Path in Bucket
-    src_path = '' if len(argv) <= 6 else argv[5]
+    src_path = '' if len(argv) <= 5 else argv[5]
     # Whether we overwrite items that already exist in destination
-    overwrite = True if len(argv) <= 7 else\
-                    {'True': True, 'False': False}[argv[7].title()]
+    overwrite = True if len(argv) <= 6 else\
+                    {'True': True, 'False': False}[argv[6].title()]
 
     download_to_path(dst_path,
                       access_key,
